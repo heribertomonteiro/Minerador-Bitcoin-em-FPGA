bbeb2c0c1c28cf084ba3a730e932a4a409613b22
Heriberto Costa Monteiro <heribertoomonteiroo@gmail.com>
2025-12-19 22:45:55 -0300

    atualização colorlight

---
+++ firmware/crt0.d
@@ -0,0 +1,3 @@
+crt0.o: /home/heriberto/fpga/litex/litex/soc/cores/cpu/picorv32/crt0.S \
+ /home/heriberto/fpga/litex/litex/soc/cores/cpu/picorv32/extraops.S
+/home/heriberto/fpga/litex/litex/soc/cores/cpu/picorv32/extraops.S:
